20f747de0ae26a5cd253398caa96eb33979af99a
shivamsprajapati13 <shivamsprajapati13@gmail.com>
2020-03-21 18:16:32 +0530

    shivam/revamp/filename(done) is completed file and other files are footer,header,clac,etc. Also in some files calculators are required i have left comment  in the code.

---
+++ distribution/shivam/revamp/feedback.php
@@ -0,0 +1,252 @@
+<html>
+<head>
+<title>Creative Finserve: Contact Us for Loan in Mumbai, Personal Loan, Business Loan, Home Loan, Term Loan, Loan Against Property, Lease Rental Discount, Car Refinance, Construction Finance, Credit Card, Creative Finserve
+</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<script type="text/javascript">
+
+function check()
+{
+     var msg="";
+   
+    if(document.f1.t1.value==0)
+    {
+        msg=msg+"Enter Your Name\n";
+    }
+    
+    if(document.f1.t2.value==0)                         // if empty textbox
+    {
+        msg=msg+"Enter mobile number \n";
+    }
+
+    if(document.f1.t2.value!=0)                         // if not a numerical textbox
+    {
+      var x=parseInt(document.f1.t2.value);
+        if(isNan(x))
+        {
+                msg=msg+"Enter valid mobile number \n";
+        }
+    }
+
+    if(document.f1.t3.value==0)
+	{
+		msg=msg + "Please Enter Email ID\n";
+	}
+       
+     if(document.f1.t3.value!=0)
+    {
+		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		var address = document.f1.t3.value;
+		if(reg.test(address) == false) 
+		{
+			msg=msg+"Please Enter Valid Email\n";
+		}
+    }
+    if(document.f1.t4.value==0)
+	{
+		msg=msg + "Please Enter Company Name\n";
+	}
+	if(document.f1.t5.value==0)
+	{
+		msg=msg + "Please Select Designation\n";
+	}
+	
+        
+
+	if(document.f1.t6.value==0)
+	{
+		msg=msg + "Please Enter Your Feedback\n"+check1();
+
+	}
+   
+    
+    
+    if(msg!="")
+	{
+		alert(msg);
+		return false;
+	}
+
+   alert(msg);
+}
+
+function check1()                                   //for dropdown validation
+{   
+    var a=document.getElementById("s1");
+    if(a.value="1")
+    {
+        alert("Please Select Profile ");
+    }
+    
+
+    
+}
+
+
+
+
+</script>
+</head>
+
+<body>
+
+<!--header-->
+
+<nav class="navbar float-top navbar-expand-lg navbar-dark bg-dark float-top">
+
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Creative Finserve</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.html">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Portfolio
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+              <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Blog
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+            </div>
+          </li>
+          <li class="nav-item active dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Other Pages
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="full-width.html">Full Width Page</a>
+              <a class="dropdown-item active" href="sidebar.html">Sidebar Page</a>
+              <a class="dropdown-item" href="faq.html">FAQ</a>
+              <a class="dropdown-item" href="404.html">404</a>
+              <a class="dropdown-item" href="pricing.html">Pricing Table</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="clearfix"></div>
+  <div class="bg1"><img src="images/feedback-inner-banner.jpg" alt="Loan in Mumbai, Personal Loan, Business Loan, Home Loan, Term Loan, Loan Against Property, Lease Rental Discount, Car Refinance, Construction Finance, Credit Card, Creative Finserve" title="Loan in Mumbai, Personal Loan, Business Loan, Home Loan, Term Loan, Loan Against Property, Lease Rental Discount, Car Refinance, Construction Finance, Credit Card, Creative Finserve" class="img-thumbnail"></div>
+<!--
+  <div class="top2_inner">	
+
+<div class="container">
+
+<div class="top2 clearfix">
+		<div class="title"><h1 style="font-weight:300;">Contact Us</h1></div>
+</div>	
+
+</div>	
+
+</div>
+
+-->
+
+<!--feedback Form Starts-->
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-5">
+       
+        <form class="form-control:focus form-control-plaintext select.form-control[multiple] h1" name="f1">
+        <br>
+        <div class="form-group" > 
+                <label class="col-form-label-lg">Enter Name <span class="mandatory">*</span></label>
+                <br>
+                <input type="textbox"   name="t1" class="form-control">  
+        </div>       
+        <br>
+        <div class="form-group">
+            <label class="col-form-label-lg">Mobile Number <span class="mandatory">*</span></label><br>
+            <input type="textbox"  maxlength="10" class="form-control" name="t2">
+        </div>
+        <br>
+        <div class="form-group">
+                <label class="col-form-label-lg"> Email Id <span class="mandatory">*</span></label><br>
+                <input type="textbox" class="form-control" name="t3">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <br>
+        <div class="form-group">
+            <label class="col-form-label-lg">Company Name <span class="mandatory">*</span></label><br>
+            <input type="textbox"  name="t4" class="form-control">
+        </div>
+        <br>
+        <div class="form-group">
+            <label class="col-form-label-lg">Designation <span class="mandatory">*</span></label><br>
+            <input type="textbox" class="form-control" name="t5">
+        </div>
+        
+        <br>
+       
+       <div class="form-group">
+        <label class="col-form-label-lg select.form-control:focus::-ms-value">Select Profile <span class="mandatory">*</span> </label><br>
+        <select  class="form-control"  id="s1">
+            <option value="1">Select Profile</option>
+            <option value="2">Salaried</option>
+            <option value="3">Self Employed</option>
+            <option value="4">Professional</option>
+            <option value="5">Other</option>
+        </select>
+        </div>
+        <br>
+        <div  class="form-group">
+            <label class="col-form-label-lg"> Feedback <span class="mandatory">*</span></label><br>
+            <textarea  class="form-control" name="t6"></textarea>
+        </div>
+
+            <br>
+            
+            <div class="align-middle">
+            <div class="g-recaptcha" data-sitekey="6LdawuIUAAAAACcM68KXlGs0KWQcOvckeXgjU5nr"></div>
+            <div>
+      
+        <br>
+        <div class="form-group"> 
+           
+        <input class="btn btn-primary" type="submit" value="Submit" onClick="check();">
+
+        </div>
+
+        </form>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
+
+
